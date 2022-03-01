bcf146333dfb9c263f327fa4a3dada2d778b02fd
IAmTomahawkx <iamtomahawkx@gmail.com>
2022-02-28 20:43:34 -0800

    update build file to install all dependancies on build

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 import platform
+from pip._internal.commands import create_command
+
+create_command("install").main(["-r", "native/requirements-dev.txt"])
+create_command("install").main(["-r", "requirements-dev.txt"])
+create_command("install").main(["-r", "requirements.txt"])
 
 import PyInstaller.__main__
-
 
 delim = ';' if platform.system() == 'Windows' else ':'
 
