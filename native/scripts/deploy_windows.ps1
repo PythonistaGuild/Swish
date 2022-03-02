bf784fdb330acaaedc67ac1b2f56be5d9938d23b
Axel <axelancerr@gmail.com>
2022-03-02 04:17:08 +0000

    remove unused native voice stuff

--- native/scripts/deploy_windows.ps1
+++
@@ -1,7 +0,0 @@
-if($env:APPVEYOR_REPO_TAG -eq 'true') {
-    Write-Output ("Deploying " + $env:APPVEYOR_REPO_TAG_NAME + " to PyPI...")
-    & "C:/Python36/python.exe" -m pip install twine
-    & "C:/Python36/python.exe" -m twine upload --skip-existing wheelhouse/*.whl
-} else {
-    Write-Output "Nothing to deploy"
-}
