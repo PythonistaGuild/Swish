5941c52fa29b2b4a56613263c4820f04c031ecec
EvieePy <evieepy@gmail.com>
2023-03-24 14:24:53 +1000

    Add version and author to init

--- swish/__init__.py
+++ swish/__init__.py
@@ -15,3 +15,5 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+__version__ = '0.0.1a'
+__author__ = 'PythonistaGuild'
